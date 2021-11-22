a5e512319156c055e99f0b1f49e6f7908d3b186c
Facundo Benitez <facundob91@gmail.com>
2021-11-22 18:29:45 -0300

    add styles to Post page

--- src/features/Posts/PostInfo.js
+++ src/features/Posts/PostInfo.js
@@ -8,16 +8,23 @@ const PostInfo = () => {
 	const post = useSelector((state) => selectPostById(state, id));
 
 	return post ? (
-		<section>
-			<article className="post">
-				<h2>{post.title}</h2>
-				<p className="post-content">{post.body}</p>
-			</article>
-		</section>
+		<>
+			<h1 className="my-4 mx-5 text-capitalize">{post.title}</h1>
+			<picture>
+				<source media="(min-width: 992px)" srcSet="https://via.placeholder.com/1050x450.webp" />
+				<source media="(min-width: 576px)" srcSet="https://via.placeholder.com/600x257.webp" />
+				<img className="w-100 h-auto my-5" src="https://via.placeholder.com/350x150.webp" />
+			</picture>
+			<p className="fs-4 mx-5">{post.body}</p>
+			<p className="fs-4 mx-5">{post.body}</p>
+			<p className="fs-4 mx-5">{post.body}</p>
+			<p className="fs-4 mx-5">{post.body}</p>
+			<p className="fs-4 mx-5">{post.body}</p>
+		</>
 	) : (
-		<section>
-			<h2>Post not found!</h2>
-		</section>
+		<>
+			<h2 className="text-center my-5 text-capitalize">Post not found!</h2>
+		</>
 	);
 };
 
--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,7 +1,12 @@
+import { Container } from 'react-bootstrap';
 import PostInfo from '../features/Posts/PostInfo';
 
 const Post = () => {
-	return <PostInfo />;
+	return (
+		<Container className="bg-secondary bg-opacity-25 py-4">
+			<PostInfo />;
+		</Container>
+	);
 };
 
 export default Post;
